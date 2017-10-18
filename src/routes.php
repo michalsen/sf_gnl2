2896a9d95be5f57e82f210e34ca45eb7ec159bdb
Eric Michalsen <eric.michalsen@gmail.com>
2017-10-18 14:24:47 -0500

    LeadMap class

--- src/routes.php
+++ src/routes.php
@@ -4,14 +4,16 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 require 'models/PostBack.php';
-
+require 'models/LeadMap.php';
 
 // POSTBACK GNL DATA
 $app->get('/postback', function (Request $request, Response $response, array $args) {
     $this->logger->info("Slim-Skeleton '/postback' route");
     $leadData = new PostBack(1413007);
+    $mapData = new LeadMap($leadData);
+
     print '<pre>';
-    print_r($leadData);
+    print_r($mapData);
 
 });
 
