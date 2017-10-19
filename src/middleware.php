7362ee65efbf19b06cdda8d16f6ecd1db58f05b2
Eric Michalsen <eric.michalsen@gmail.com>
2017-10-19 08:07:14 -0500

    middleware

--- src/middleware.php
+++ src/middleware.php
@@ -3,9 +3,13 @@
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
 
+
+## DotENV
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
+
+## Redean ORM
 use \RedBeanPHP\R as R;
 $host = 'localhost';
 $user = $_ENV['DB_USER'];
@@ -13,3 +17,15 @@ $pass = $_ENV['DB_PASS'];
 $db = $_ENV['DB_NAME'];
 R::setup("mysql:host=$host;dbname=$db", $user, $pass);
 
+
+## Salesforce
+$SFbuilder = new \Phpforce\SoapClient\ClientBuilder(
+  __DIR__ . '/wsdl/enterprise.wsdl.xml',
+  $_ENV['SF_USER'],
+  $_ENV['SF_PASS'],
+  $_ENV['SF_API']
+);
+
+
+## Calendar
+$calendar = $_ENV['CAL_URL'];
